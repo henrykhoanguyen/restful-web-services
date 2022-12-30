6eb4274c1f7d9e0769bc3283d7b880b05a84eee8
Khoa Nguyen <henrykhoanguyen@gmail.com>
2022-12-30 14:45:32 -0600

    creat hello world

--- src/main/java/com/rest/webservices/restfulwebservices/helloworld/HelloWorldController.java
+++ src/main/java/com/rest/webservices/restfulwebservices/helloworld/HelloWorldController.java
@@ -1,4 +1,18 @@
 package com.rest.webservices.restfulwebservices.helloworld;
 
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+// Rest API
+@RestController
 public class HelloWorldController {
+
+    // hello world
+    @GetMapping(path = "/hello-world")
+    public String helloWorld(){
+        return "Hello World";
+    }
 }
