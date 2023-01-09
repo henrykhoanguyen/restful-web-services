8420a244b6a6a70bde69a3dfd1d2575e0559d8f2
Khoa Nguyen <henrykhoanguyen@gmail.com>
2023-01-08 19:40:06 -0600

    added security

--- src/main/java/com/rest/webservices/restfulwebservices/user/UserJPAResource.java
+++ src/main/java/com/rest/webservices/restfulwebservices/user/UserJPAResource.java
@@ -6,8 +6,12 @@ import jakarta.validation.Valid;
 import org.springframework.hateoas.EntityModel;
 import org.springframework.hateoas.server.mvc.WebMvcLinkBuilder;
 import org.springframework.http.ResponseEntity;
-import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import java.net.URI;
@@ -63,11 +67,12 @@ public class UserJPAResource {
 
     @DeleteMapping("/jpa/users/{id}")
     public void deleteUser(@PathVariable int id){
+        System.out.println("id: " + id);
         userRepository.deleteById(id);
     }
 
     @PostMapping("/jpa/users")
-    public ResponseEntity<User> createUser(@Validated @RequestBody User user){
+    public ResponseEntity<User> createUser(@Valid @RequestBody User user){
         User savedUser = userRepository.save(user);
 
         URI location = ServletUriComponentsBuilder
